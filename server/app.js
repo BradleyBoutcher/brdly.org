2e73553858e4df0f3beae7f8bb640acc0cc24b61
Bradley <bboutcher@vmware.com>
2019-10-03 16:16:03 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -42,6 +42,7 @@ app.use(function(req, res, next) {
   // TODO Add origin validation
   res.header('Access-Control-Allow-Origin', origin);
   res.header('Access-Control-Allow-Origin', "https://brdly.com");
+  res.header('Access-Control-Allow-Origin', "localhost:4000");
   res.header('Access-Control-Allow-Methods', 'POST, PUT, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Cache-Control, Pragma');
 
