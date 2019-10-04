5cb308dc905df1951b25b90f350c1deb85930c0f
Bradley <bboutcher@vmware.com>
2019-10-04 11:42:13 -0400

    Polish

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 var bodyParser = require('body-parser');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
